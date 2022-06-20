96ab4610f46801c447b21118554605c1cb963350
Akpanuwa Udeme <89720943+AkpanuwaUdeme@users.noreply.github.com>
2022-06-20 07:31:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/* First, add the code here */
+
+cat_image = document.querySelector('img');
+
+cat_image.addEventListener('click', ()=> {
+ 
+  // add css that makes the cat dissapear
+})
+
+
+
+
+
+
+
+setTimeout(() => {  document.querySelector('.temp').style = "display: none"
+
+/*  Then, move the code here instead. */
+  
+}, 3000)
